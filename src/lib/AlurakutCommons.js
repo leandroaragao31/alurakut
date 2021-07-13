dd3f6d56ff91b2ca40d6f5a7a753cc3856f4ad7d
leandroaragao31 <leandroaragao31@gmail.com>
2021-07-13 03:36:25 -0300

    alterando estilização na lib

--- src/lib/AlurakutCommons.js
+++ src/lib/AlurakutCommons.js
@@ -54,7 +54,7 @@ export function AlurakutMenu({ githubUser }) {
 }
 AlurakutMenu.Wrapper = styled.header`
   width: 100%;
-  background-color: #308BC5;
+  background-color: #00BFFF ;
   .alurakutMenuProfileSidebar {
     background: white;
     position: fixed;
@@ -93,7 +93,7 @@ AlurakutMenu.Wrapper = styled.header`
     }
   }
   .container {
-    background-color: #308BC5;
+    background-color: #00BFFF;
     padding: 7px 16px;
     max-width: 1110px;
     margin: auto;
@@ -140,7 +140,7 @@ AlurakutMenu.Wrapper = styled.header`
     }
     input {
       color: #ffffff;
-      background: #5579A1;
+      background: #4F94CD;
       padding: 10px 42px;
       border: 0;
       background-image: url(${`${BASE_URL}/icons/search.svg`});
