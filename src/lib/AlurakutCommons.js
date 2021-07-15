2c3940fa6d804842e50c7687f411b05ccfc829c3
leandroaragao31 <leandroaragao31@gmail.com>
2021-07-15 03:10:54 -0300

    alterando sidebar photo

--- src/lib/AlurakutCommons.js
+++ src/lib/AlurakutCommons.js
@@ -172,7 +172,7 @@ function AlurakutMenuProfileSidebar({ githubUser }) {
   return (
     <div className="alurakutMenuProfileSidebar">
       <div>
-        <img src={`https://github.com/leandroaragao31.png`} style={{ borderRadius: '8px' }} />
+        <img src={`https://github.com/${githubUser}.png`} style={{ borderRadius: '8px' }} />
         <hr />
         <p>
           <a className="boxLink" href={`/user/${githubUser}`}>
